807d5a9c545e6fcdc9b2f318f601a5abb4032e7f
Spencerp34 <spencerp34@yahoo.com>
2021-07-26 12:19:52 -0600

    set up routing and wireframe with some fake dummydata

--- src/App.js
+++ src/App.js
@@ -1,10 +1,53 @@
-import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+// import Login from "./components/Login";
+import Dashboard from "./components/dashboard";
+// import PrivateRoute from "./components/PrivateRoute";
+import { axiosWithAuth } from "./helpers/axiosWithAuth";
+import Home from './components/home.js';
 
 function App() {
+
+  const logout = () => {
+    // axiosWithAuth()
+    // .post('/logout')
+    // .then(res => {
+      localStorage.removeItem('token');
+      localStorage.setItem('username');
+      localStorage.setItem('role');
+      window.location.href = "/login";
+      console.log('logged out')
+    // })
+    // .catch(err => {
+    //   console.log(err);
+    //   localStorage.removeItem('token');
+    //   window.location.href = "/login";
+    // })
+  };
+
+
+
   return (
-    <div className="App">
-      <h1>Testing</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <header>
+          Anywhere Fitness!
+          <div data-testid="logoutButton" className='logout'>
+            {localStorage.getItem('token') ? <Link onClick={logout} to='/home'>logout</Link> : <div></div>}
+          </div>
+        </header> 
+
+        <Route exact path="/home" component={Home} />
+
+        <Route exact path='/login' component={null} />
+
+        <Route exact path='/signUp' component={null} />
+
+        <Route exact path='/dashboard' component={Dashboard} />
+
+        <Route exact path='/instructorDashboard' component={Dashboard} />
+      </div>
+    </Router>
   );
 }
 
